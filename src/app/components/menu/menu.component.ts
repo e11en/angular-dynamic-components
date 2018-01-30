96ccd8839a317bfdc7a7cfa2426a88b7293137d2
Ellen Langelaar <elangelaar@thinkwisesoftware.com>
2018-01-30 17:35:03 +0100

    Added path text to menu bar

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 
 import { ComponentService } from '@app/services/component.service';
 
@@ -12,15 +13,27 @@ import { ComponentService } from '@app/services/component.service';
         <a [routerLink]="menuItem.link">{{ menuItem.text }}</a>
       </li>
     </ul>
+
+    <p>URL: {{currentUrl}}</p>
+    <div class="clear-fix"></div>
   </div>
   `
 })
 export class MenuComponent implements OnInit {
-  private menuItems = [];
+  menuItems = [];
+  currentUrl: string;
 
-  constructor(private componentService: ComponentService) { }
+  constructor(private componentService: ComponentService,
+              private router: Router) { }
 
   ngOnInit() {
     this.menuItems = this.componentService.getEntityLinks();
+    this.currentUrl = this.router.url;
+
+    this.router.events.forEach(event => {
+      if (event instanceof NavigationStart) {
+        this.currentUrl = event.url;
+      }
+    });
   }
 }
